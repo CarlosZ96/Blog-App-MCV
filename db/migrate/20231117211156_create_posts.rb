5cfae663b969f3f3c881a55e049d5c2c84a519ff
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-11-17 19:08:59 -0500

    Update 20231117211156_create_posts.rb
    review fix createpost.

--- db/migrate/20231117211156_create_posts.rb
+++ db/migrate/20231117211156_create_posts.rb
@@ -1,20 +1,13 @@
-class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :likes_counter, default: 0
+      t.integer :comments_counter, default: 0
+      t.references :author, foreign_key: { to_table: 'users' }
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  after_save :update_user_post_counter
-
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  private
-
-  def update_user_post_counter
-    user.update(posts_counter: user.posts.count)
+      t.timestamps
+    end
   end
 end
