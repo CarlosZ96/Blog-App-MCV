de71e3ff3e4358e3a7a126e4ba347da448d129e4
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-11-29 16:30:06 -0500

    Feature:
    individual user view created

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'users', to: 'users#index', as: :users
+  get 'users/:id', to: 'users#show', as: :user
 end
