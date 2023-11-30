f7955d0b5a5cfd779c97ac015f2611c6b27f7f75
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-11-29 20:37:20 -0500

    Update rails_helper.rb
    fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
 
