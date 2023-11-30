db8887af0d9c2c260ec6dc55c30d4ebcfb514b11
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-11-29 19:33:48 -0500

    Feature:
    Creating actions and views for all 4 URLs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
-    @user =  User.find(params[:id])
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   get 'users', to: 'users#index', as: :users
   get 'users/:id', to: 'users#show', as: :user
+  get 'users/:id/posts', to: 'posts#index', as: :posts
+  get 'users/:id/posts/:id', to: 'posts#show', as: :post
 end
